c2d0fa1fca2f344800021af5764df4d45dbcaa0d
row-huh <rohaabropathan@gmail.com>
2025-07-21 10:23:38 +0500

    encode and decode done

--- Simpletokenizer.py
+++ Simpletokenizer.py
@@ -8,8 +8,12 @@ class SimpleTokenizerV1:
     def encode(self,text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
 
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
+    
+
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import re
+from Simpletokenizer import SimpleTokenizerV1
 
 with open("the-verdict.txt", "r", encoding="utf-8") as file:
     raw_text = file.read()
 
-print("Total number of characters: ", len(raw_text))
-print(raw_text[:99])
 
+# preprocessing before assiginin token ids
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
-
 all_words = sorted(set(preprocessed))
-vocab_size = len(all_words)
-print(vocab_size)
-
 
 vocab = {token:integer for integer, token in enumerate(all_words)}
+print("Pre tokenization: ", vocab)
 
-for i, item in enumerate(vocab.items()):
-    print(item)
-    if i >= 50:
-        break
+tokenizer = SimpleTokenizerV1(vocab=vocab)
+text = """
+    "It's the last he painted, you know,"
+    Mrs. Gisburn said with pardonable pride
+"""
+
+ids = tokenizer.encode(text)
+print(ids)
+
+print(tokenizer.decode(ids))
