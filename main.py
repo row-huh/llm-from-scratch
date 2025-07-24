7b8c553a75b7ee65ca61b04747b74afa3d13cef3
row-huh <rohaabropathan@gmail.com>
2025-07-24 16:01:56 +0500

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from util import SimpleTokenizerV1, SimpleTokenizerV2, DataLoader
+from util import SimpleTokenizerV1, SimpleTokenizerV2, create_dataloader_v1
 import tiktoken
 from torch.utils.data import DataLoader
 import torch
@@ -38,17 +38,3 @@ print(tokenizer.encode(text))
 print(tokenizer.decode(tokenizer.encode(text)))
 
 
-
-def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
-    tokenizer = tiktoken.get_encoding("gpt2")
-    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
-
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers
-    )
-
-    return dataloader
