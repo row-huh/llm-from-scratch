ca59ec4243b943475ee4ba10f6b22e10d540a304
row-huh <rohaabropathan@gmail.com>
2025-07-24 16:00:02 +0500

    moved `create_dataloader_v1` class into util.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import re
-from util import SimpleTokenizerV1, SimpleTokenizerV2
+from util import SimpleTokenizerV1, SimpleTokenizerV2, DataLoader
+import tiktoken
+from torch.utils.data import DataLoader
+import torch
+
 
 with open("the-verdict.txt", "r", encoding="utf-8") as file:
     raw_text = file.read()
@@ -32,3 +36,19 @@ print(text)
 print(tokenizer.encode(text))
 
 print(tokenizer.decode(tokenizer.encode(text)))
+
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
--- util.py
+++ util.py
@@ -39,7 +39,7 @@ class SimpleTokenizerV2:
 
 import torch
 from torch.utils.data import Dataset, DataLoader
-
+import tiktoken
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
@@ -60,3 +60,19 @@ class GPTDatasetV1(Dataset):
     
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
+    
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
