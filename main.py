011a71c30f8a9f4226c3aacca6e73dbe988002e5
row-huh <rohaabropathan@gmail.com>
2025-07-25 10:13:03 +0500

    embedding vectors

--- main.py
+++ main.py
@@ -32,13 +32,8 @@ text1 = "Hello, do you like tea?"
 text2 = "In the sunlit terraces of the palace."
 text = " <|endoftext|> ".join((text1, text2))
 
-print(text)
 
-print(tokenizer.encode(text))
-
-print(tokenizer.decode(tokenizer.encode(text)))
-
-with open("the-verditct.tx", "r", encoding="utf-8") as f:
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 dataloader = create_dataloader_v1(
@@ -47,4 +42,23 @@ dataloader = create_dataloader_v1(
 
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
-print(first_batch)
+second_batch = next(data_iter)
+#print("First batch tensor:", first_batch)
+#print("Second batch tensor:", second_batch)
+
+
+# testing embeddings
+input_ids = torch.tensor([2, 3, 5, 1])
+
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+#print(embedding_layer.weight)
+
+# apply to a token id 3
+#print(embedding_layer(torch.tensor([3])))
+
+# embedding layer of all input ids
+print(embedding_layer(input_ids))
