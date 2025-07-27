2fdd08bc5a06e98be37dd9af9a437013dec36492
row-huh <rohaabropathan@gmail.com>
2025-07-27 11:30:23 +0500

    created another embedding layer

--- main.py
+++ main.py
@@ -62,3 +62,25 @@ embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
 # embedding layer of all input ids
 print(embedding_layer(input_ids))
+
+
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length,
+    stride=max_length, shuffle=False
+    )
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs: \n", inputs)
+print("\nInputs shape: \n", inputs.shape)
+
+# creating another embedding layer
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arrange(context_length))
+# this shows embeddings
+print(pos_embeddings.shape)
