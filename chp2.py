0f5c20b9b9597f21183a407171ced41b03072450
row-huh <rohaabropathan@gmail.com>
2025-07-29 15:16:56 +0500

    2.4

--- chp2.py
+++ chp2.py
@@ -7,3 +7,7 @@
 
 # enumerate each item and store in a dict object (call it vocabulary)
 
+# introduce <| unk |> and <| end of text|> special tokens into your vocabulary as well
+
+# create a class that takes in text, creates a vocab, tokenizes other text
+# based on that vocab and if any word doesnt exist, it will use a special token
