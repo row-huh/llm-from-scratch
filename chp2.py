7cfcf17eea622cb620317a04d6e9fafdbf9752ba
row-huh <rohaabropathan@gmail.com>
2025-07-29 15:02:21 +0500

    2.3

---
+++ chp2.py
@@ -0,0 +1,9 @@
+# read the-verdict
+
+
+# tokenize (using the regex approach)
+
+# sort all and convert into set to remove duplicates
+
+# enumerate each item and store in a dict object (call it vocabulary)
+
