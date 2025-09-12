e0e591f10f867137d7dbaf3ce2b48f286670e682
row-huh <rohaabropathan@gmail.com>
2025-09-12 18:56:03 +0500

    .

--- src/highlevelgptoverview.py
+++ src/highlevelgptoverview.py
@@ -16,8 +16,23 @@ batch = torch.stack(batch, dim=0)
 
 print(batch)
 
+
 torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch) # same as model.forward(batch). Why?? because yes
 print("Output shape:", logits.shape)
 print(logits)
+
+# example of figure 4.5 (page 100)
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(type(out))
+
+# calculating mean of this small neural network and variance it - they will later be normalized
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
