84dc468ffcdc9fefa5a2ac424a57dcf0443c68eb
row-huh <rohaabropathan@gmail.com>
2025-09-16 09:24:30 +0500

    .

--- src/DummyGPTModel.py
+++ src/DummyGPTModel.py
@@ -11,6 +11,40 @@ GPT_CONFIG_124M = {
     "qkv_bias": False,  # to be implemented in chapter 6
 }
 
+
+
+import torch
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+        
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        return queries, keys, values
+
+
+
 # nn.module is base clas for neural networks
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
@@ -91,4 +125,34 @@ class FeedForward(nn.Module):
     
     def forward(self, x):
         return self.layers(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg["embd_dim"],
+            d_out = cfg["embd_dim"],
+            context_length = cfg["context_length"],
+            num_heads = cfg["n_heads"],
+            dropout = cfg["drop_rate"],
+            qkv_bias = cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["embd_dim"])
+        self.norm2 = LayerNorm(cfg["embd_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+        
+        
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        
+        shortcut = x
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
     
