8e03739b058950dd9ce4c8eeaa96a7546161b7f8
row-huh <rohaabropathan@gmail.com>
2025-09-18 14:14:41 +0500

    .

--- src/GPT.py
+++ src/GPT.py
@@ -170,6 +170,18 @@ class GPTModel(nn.Module):
         return logits
     
     
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size]
+        with torch.no_grad():
+            logits = model(idx_cond)
+            
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    
+    
 # creating a input batch first
 tokenizer = tiktoken.get_encoding("gpt2")
 batch = []
