c01616ae316f842afbc3f5397d9f75f46256a8c4
row-huh <rohaabropathan@gmail.com>
2025-09-30 08:14:12 +0500

    .

--- downloadcode.py
+++ downloadcode.py
@@ -1,8 +1,19 @@
-import urllib.request
-url = (
-"https://raw.githubusercontent.com/rasbt/"
-"LLMs-from-scratch/main/ch05/"
-"01_main-chapter-code/gpt_download.py"
+# import urllib.request
+# url = (
+# "https://raw.githubusercontent.com/rasbt/"
+# "LLMs-from-scratch/main/ch05/"
+# "01_main-chapter-code/gpt_download.py"
+# )
+# filename = url.split('/')[-1]
+# urllib.request.urlretrieve(url, filename)
+
+from gpt_download import download_and_load_gpt2
+settings, params = download_and_load_gpt2(
+    model_size="124M", models_dir="gpt2"
 )
-filename = url.split('/')[-1]
-urllib.request.urlretrieve(url, filename)
+
+print("Settings", settings)
+print("Parameter dictionary keys", params.keys())
+
+print(params["wte"])
+print("Token embedding weight tensor dimensions: ", params["wte"].shape)
